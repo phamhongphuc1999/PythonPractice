f6c0e563d83f1eec6f5bc87969b648bc8fa647ef
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2021-10-08 21:50:34 +0700

    :art: create colorfull logging message

---
+++ src/logging/colorfull_logging.py
@@ -0,0 +1,50 @@
+import logging
+from color_config import fgCyan, fgWhite, fgGreen, fgYellow, fgRed, reset, bold, fgMagenta
+
+COLORS = {
+    "DEBUG": fgWhite,
+    "INFO": fgGreen,
+    "WARNING": fgYellow,
+    "ERROR": fgRed,
+    "CRITICAL": fgMagenta
+}
+
+
+class ColoredFormatter(logging.Formatter):
+    def __init__(self, msg, use_color=True):
+        logging.Formatter.__init__(self, msg)
+        self.use_color = use_color
+
+    def format(self, record):
+        level_name = record.levelname
+        if self.use_color and level_name in COLORS:
+            level_name_color = COLORS[level_name] + f"[{level_name}]" + reset
+            record.levelname = level_name_color
+        return logging.Formatter.format(self, record)
+
+
+# Custom logger class with multiple destinations
+class ColoredLogger(logging.Logger):
+    FORMAT = f"{bold}[%(asctime)s]{reset} %(levelname)s: {bold}%(message)s{reset} {fgCyan}(%(filename)s:%(lineno)d){reset}"
+
+    def __init__(self, name):
+        logging.Logger.__init__(self, name, logging.DEBUG)
+
+        color_formatter = ColoredFormatter(self.FORMAT)
+
+        console = logging.StreamHandler()
+        console.setFormatter(color_formatter)
+
+        self.addHandler(console)
+        return
+
+
+logging.setLoggerClass(ColoredLogger)
+
+if __name__ == "__main__":
+    abc = ColoredLogger("abc")
+    abc.debug("This is debug")
+    abc.info("This is info")
+    abc.warning("This is warning")
+    abc.error("This is error")
+    abc.critical("this is critical")
--- src/logging/test_logging.py
+++ src/logging/test_logging.py
@@ -1,11 +1,11 @@
 import logging
 
-from src.logging import MY_LOG, INFO, DEBUG, MyLogger
+from src.logging import MY_LOG, INFO, DEBUG, MyLogger, CustomFormatter
 
 trava_logger = MyLogger("my_logger")
 
 
-def setup_trava_logging(file_log_level=INFO, console_log_level=MY_LOG):
+def setup_trava_logging(file_log_level=INFO, console_log_level=DEBUG):
     """
     Setup trava logging
 
@@ -15,13 +15,15 @@ def setup_trava_logging(file_log_level=INFO, console_log_level=MY_LOG):
     log_formatter = logging.Formatter(
         '[%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s', '%m-%d %H:%M:%S')
 
-    file_handler = logging.FileHandler("trava_api/logging/log-file.log")
-    file_handler.setFormatter(log_formatter)
+    custom_formatter = CustomFormatter()
+
+    file_handler = logging.FileHandler("log-file.log")
+    file_handler.setFormatter(custom_formatter)
     file_handler.setLevel(file_log_level)
     trava_logger.addHandler(file_handler)
 
     console_handler = logging.StreamHandler()
-    console_handler.setFormatter(log_formatter)
+    console_handler.setFormatter(custom_formatter)
     console_handler.setLevel(console_log_level)
     trava_logger.addHandler(console_handler)
 
@@ -30,3 +32,5 @@ def setup_trava_logging(file_log_level=INFO, console_log_level=MY_LOG):
 
 if __name__ == "__main__":
     setup_trava_logging(file_log_level=INFO, console_log_level=MY_LOG)
+    trava_logger.info("12345")
+    trava_logger.error("123455555")
